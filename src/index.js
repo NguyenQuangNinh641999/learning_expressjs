b48afb0c5c81b57c3f000c74c7e0df1902022b56
NguyenQuangNinh641999 <ninh.nguyen@tomosia.com>
2022-09-14 22:58:30 +0700

    first

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(morgan('combined'))
 
@@ -15,8 +16,21 @@ app.set('views',path.join(__dirname,'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.get('/search', (req, res) => {
+  // console.log(req.query.q);
+  res.render('search');
+});
+
+// app.post('/search', (req, res) => {
+//   // console.log(req.query.q);
+//   res.render('search');
+// });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
